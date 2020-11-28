7a080a458d3fd0161b721f246d76ffcaa4fe6a55
Tsakaleris Konstantinos <dai18071@uom.edu.gr>
2020-11-28 23:13:30 +0200

    added log in activity ui

--- app/src/main/java/com/example/cerberus/MainActivity.java
+++ app/src/main/java/com/example/cerberus/MainActivity.java
@@ -3,14 +3,28 @@ package com.example.cerberus;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        Button buttonDefaults = findViewById(R.id.buttonDefaults);
+        buttonDefaults.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, LogInActivity.class);
+        if (intent != null) {
+            startActivity(intent);
+        }
     }
 }
