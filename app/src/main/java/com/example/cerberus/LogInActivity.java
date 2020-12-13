c9433c485c5e03d7388939515cdcfcc0dde1a5ef
Tsakaleris Konstantinos <dai18071@uom.edu.gr>
2020-12-13 21:57:57 +0200

    added facebook login

--- app/src/main/java/com/example/cerberus/LogInActivity.java
+++ app/src/main/java/com/example/cerberus/LogInActivity.java
@@ -3,12 +3,19 @@ package com.example.cerberus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Result;
@@ -21,10 +28,17 @@ import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LogInActivity extends AppCompatActivity {
 
     public static final String TAG = "Twitter";
-    public TwitterLoginButton loginButton;
+    private TwitterLoginButton loginButton;
+    private LoginButton fbLoginButton;
+    private CallbackManager callbackManager;
+
+    //REMEMBER THE STRINGS!!!
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +49,9 @@ public class LogInActivity extends AppCompatActivity {
                 .debug(true)
                 .build();
         Twitter.initialize(config);
-        //Twitter.initialize(this);
         setContentView(R.layout.activity_log_in);
 
-        loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_button);
         loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
@@ -56,13 +69,70 @@ public class LogInActivity extends AppCompatActivity {
                 exception.printStackTrace();
             }
         });
+
+        fbLoginButton = (LoginButton) findViewById(R.id.facebook_button);
+        fbLoginButton.setPermissions("email", "public_profile");
+        callbackManager = CallbackManager.Factory.create();
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                String userId = loginResult.getAccessToken().getUserId();
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        displayUserInfo(object);
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "first_name, last_name, email, id");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    public void displayUserInfo(JSONObject object) {
+        String firstName, lastName, email, id;
+        firstName = "";
+        lastName = "";
+        email = "";
+        id = "";
+        try {
+            firstName = object.getString("first_name");
+            lastName = object.getString("last_name");
+            email = object.getString("email");
+            id = object.getString("id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        TextView tv_name, tv_email, tv_id;
+        tv_name = findViewById(R.id.tv_name);
+        tv_email = findViewById(R.id.tv_email);
+        tv_id = findViewById(R.id.tv_id);
+
+        tv_name.setText(firstName + lastName);
+        tv_email.setText(email);
+        tv_id.setText(id);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Pass the activity result to the login button.
+        // Pass the activity result to the login button. //twitter
         loginButton.onActivityResult(requestCode, resultCode, data);
+        //facebook
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
