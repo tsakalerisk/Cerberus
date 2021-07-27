806513b72235135ac3414900b7d2d43c987b4fb6
Konstantinos Tsakaleris <kontsakaleris@gmail.com>
2021-07-28 00:26:15 +0300

    cleaned-up summer version

--- app/src/main/java/com/example/cerberus/LogInActivity.java
+++ app/src/main/java/com/example/cerberus/LogInActivity.java
@@ -1,12 +1,15 @@
 package com.example.cerberus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -16,123 +19,96 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.twitter.sdk.android.core.Callback;
-import com.twitter.sdk.android.core.DefaultLogger;
-import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterAuthToken;
-import com.twitter.sdk.android.core.TwitterConfig;
-import com.twitter.sdk.android.core.TwitterCore;
-import com.twitter.sdk.android.core.TwitterException;
-import com.twitter.sdk.android.core.TwitterSession;
-import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import twitter4j.Twitter;
+import twitter4j.auth.RequestToken;
+
 public class LogInActivity extends AppCompatActivity {
 
-    public static final String TAG = "Twitter";
-    private TwitterLoginButton loginButton;
-    private LoginButton fbLoginButton;
-    private CallbackManager callbackManager;
-
-    //REMEMBER THE STRINGS!!!
+    public static final String TAG_TWITTER = "Twitter";
+    public static final String TAG_FACEBOOK = "Facebook";
+    private LoginButton fbLoginButton = null;
+    private ImageView fbImage = null;
+    private TextView fbName = null;
+    private Button twLoginButton = null;
+    private ImageView twImage = null;
+    private TextView twName = null;
+    private CallbackManager callbackManager = null;
+    private Twitter twitter = null;
+    private RequestToken requestToken = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TwitterConfig config = new TwitterConfig.Builder(this)
-                .logger(new DefaultLogger(Log.DEBUG))
-                .twitterAuthConfig(new TwitterAuthConfig(BuildConfig.TwitterApiKey, BuildConfig.TwitterApiKeySecret))
-                .debug(true)
-                .build();
-        Twitter.initialize(config);
+
         setContentView(R.layout.activity_log_in);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        findAllViews();
+        setFacebookCallback();
+    }
 
-        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_button);
-        loginButton.setCallback(new Callback<TwitterSession>() {
-            @Override
-            public void success(Result<TwitterSession> result) {
-                // Do something with result, which provides a TwitterSession for making API calls
-                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
-                TwitterAuthToken authToken = session.getAuthToken();
-                String token = authToken.token;
-                String secret = authToken.secret;
-            }
-
-            @Override
-            public void failure(TwitterException exception) {
-                Toast.makeText(LogInActivity.this,"FAILED TO LOG IN!", Toast.LENGTH_LONG).show();
-                Log.e(TAG, "STACK TRACE FOLLOWING");
-                exception.printStackTrace();
-            }
-        });
-
-        fbLoginButton = (LoginButton) findViewById(R.id.facebook_button);
+    private void setFacebookCallback() {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.setApplicationId(BuildConfig.FacebookAppId);
         fbLoginButton.setPermissions("email", "public_profile");
         callbackManager = CallbackManager.Factory.create();
         fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                String userId = loginResult.getAccessToken().getUserId();
+                Log.d(TAG_FACEBOOK, "Success!");
+                /*String userId = loginResult.getAccessToken().getUserId();
                 GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response) {
-                        displayUserInfo(object);
+                        displayFacebookInfo(object);
                     }
                 });
                 Bundle parameters = new Bundle();
-                parameters.putString("fields", "first_name, last_name, email, id");
+                parameters.putString("fields", "first_name, last_name, picture.type(large)");
                 graphRequest.setParameters(parameters);
-                graphRequest.executeAsync();
+                graphRequest.executeAsync();*/
             }
 
             @Override
             public void onCancel() {
-
+                Log.e(TAG_FACEBOOK, "Cancelled");
             }
 
             @Override
             public void onError(FacebookException error) {
-
+                error.printStackTrace();
+                Log.e(TAG_FACEBOOK, error.toString());
             }
         });
     }
 
-    public void displayUserInfo(JSONObject object) {
-        String firstName, lastName, email, id;
+    protected void findAllViews() {
+        fbLoginButton = findViewById(R.id.fbButton);
+        fbImage = findViewById(R.id.fbImage);
+        fbName = findViewById(R.id.fbName);
+
+        //twLoginButton = findViewById(R.id.twButton);
+        twImage = findViewById(R.id.twImage);
+        twName = findViewById(R.id.twName);
+    }
+
+    public void displayFacebookInfo(JSONObject object) {
+        String firstName, lastName;
         firstName = "";
         lastName = "";
-        email = "";
-        id = "";
         try {
             firstName = object.getString("first_name");
             lastName = object.getString("last_name");
-            email = object.getString("email");
-            id = object.getString("id");
+            String imageUrl = object.getJSONObject("picture").getJSONObject("data").getString("url");
+            Picasso.get().load(imageUrl).into(fbImage);
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        TextView tv_name, tv_email, tv_id;
-        tv_name = findViewById(R.id.tv_name);
-        tv_email = findViewById(R.id.tv_email);
-        tv_id = findViewById(R.id.tv_id);
-
-        tv_name.setText(firstName + lastName);
-        tv_email.setText(email);
-        tv_id.setText(id);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Pass the activity result to the login button. //twitter
-        loginButton.onActivityResult(requestCode, resultCode, data);
-        //facebook
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        fbName.setText(firstName + " " + lastName);
     }
 }
